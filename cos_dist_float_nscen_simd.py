47e484b11096cb6e98d76bd89d55417ec526bfd4
Chiem <c.p.stevens@student.tue.nl>
2024-06-04 10:56:19 +0200

    Another way of generating shares.

--- cos_dist_float_nscen_simd.py
+++ cos_dist_float_nscen_simd.py
@@ -9,9 +9,7 @@ pffrocd.OUTPUT_FILE_NAME = f"output_{pffrocd.EXECUTABLE_NAME}.txt"
 
 # get two embeddings of different people
 x,y=pffrocd.get_two_random_embeddings(False)
-r = pffrocd.generate_nonce(y, dtype=np.float32)
-y1 = r
-y0 = pffrocd.fxor(y, y1, dtype=np.float32)
+share0, share1 = pffrocd.create_shares(y, dtype=np.float32)
 
 # Print out the cosine distance for verificaiton before the normalization happens
 print("NUMPY COS_DIST:")
@@ -26,7 +24,7 @@ print("Normalize COS_DIST:")
 print(1 - np.dot(x, y))
 
 # Run the circuit
-output = pffrocd.run_sfe(x, y, y_0=y0, y_1=y1)
+output = pffrocd.run_sfe(x, y, y_0=share0, y_1=share1)
 
 
 # get two embeddings of different people
