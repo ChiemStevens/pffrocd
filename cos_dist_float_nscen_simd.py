dba1e0861df8ef10834f0ab46356eb69976bd189
Chiem <c.p.stevens@student.tue.nl>
2024-06-05 10:43:02 +0200

    Convert to array to see if it fixes the 0d array problem

--- cos_dist_float_nscen_simd.py
+++ cos_dist_float_nscen_simd.py
@@ -9,7 +9,11 @@ pffrocd.OUTPUT_FILE_NAME = f"output_{pffrocd.EXECUTABLE_NAME}.txt"
 
 # get two embeddings of different people
 x,y=pffrocd.get_two_random_embeddings(False)
+#convert x to np.ndarray
+x = np.array(x, dtype=np.float32)
+y = np.array(y, dtype=np.float32)
 share0, share1 = pffrocd.create_shares(y, dtype=np.float32)
+share0prime, share1prime = pffrocd.create_shares(share0, dtype=np.float32)
 
 # Print out the cosine distance for verificaiton before the normalization happens
 print("NUMPY COS_DIST:")
@@ -25,23 +29,6 @@ print(1 - np.dot(x, y))
 
 # Run the circuit
 output = pffrocd.run_sfe(x, y, y_0=share0, y_1=share1)
-
-
-# get two embeddings of different people
-# print("Getting two random embeddings...")
-# x, y = pffrocd.get_two_random_embeddings(False)
-# print("got the embeddings!")
-
-# r = pffrocd.generate_nonce(y)
-
-# y1 = r
-
-# y0 = pffrocd.fxor(y, y1)
-
-
-# output = pffrocd.run_sfe(x, y, y_0=y0, y_1=y1)
-
-# print(output.stdout)
 
 # Print the output
 print(output.stdout)
