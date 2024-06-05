1340b39c1baa7d07e251eae6c98f542302aca981
Chiem <c.p.stevens@student.tue.nl>
2024-06-05 11:07:59 +0200

    The -o in run_sfe does not stand for output file but the absolute path to the pffrocd directory, changed accordingly

--- cos_dist_float_nscen_simd.py
+++ cos_dist_float_nscen_simd.py
@@ -5,7 +5,7 @@ import numpy as np
 pffrocd.EXECUTABLE_PATH = "ABY/build/bin"
 pffrocd.EXECUTABLE_NAME = 'cos_dist_float_scen_simd_32'
 pffrocd.INPUT_FILE_NAME = f"input_{pffrocd.EXECUTABLE_NAME}.txt"
-pffrocd.OUTPUT_FILE_NAME = f"output_{pffrocd.EXECUTABLE_NAME}.txt"
+pffrocd.OUTPUT_FILE_NAME = "/home/chiem/pffrocd"
 
 # get two embeddings of different people
 x,y=pffrocd.get_two_random_embeddings(False)
@@ -13,7 +13,7 @@ x,y=pffrocd.get_two_random_embeddings(False)
 x = np.array(x, dtype=np.float32)
 y = np.array(y, dtype=np.float32)
 share0, share1 = pffrocd.create_shares(y, dtype=np.float32)
-share0prime, share1prime = pffrocd.create_shares(share0, dtype=np.float32)
+# share0prime, share1prime = pffrocd.create_shares(share0, dtype=np.float32)
 
 print("share0:", share0)
 
