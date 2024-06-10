89aafaa880f44a0a960c2d4f80ccba8f3873f5d6
Chiem <c.p.stevens@student.tue.nl>
2024-06-10 11:54:55 +0200

    added print for verification

--- pyscripts/test-ar_circuit-calcs.py
+++ pyscripts/test-ar_circuit-calcs.py
@@ -3,14 +3,18 @@
 
 # output :
 # Result = x0 * x1 * x2;
+def get_cos_dist_numpy(x, y):
+    return 1 - np.dot(x, y)/(np.linalg.norm(x)*np.linalg.norm(y))
 
+def get_cos_dist_nom(x, y):
+    return 1 - np.dot(x, y)
 
 import pffrocd # helper functions
 import numpy as np
 
 # get two random int vectors
-x = pffrocd.get_embedding("/home/chiem/pffrocd/pfw/German_Khan/German_Khan_0001.jpg")
-y = pffrocd.get_embedding("/home/chiem/pffrocd/pfw/Gina_Centrello/Gina_Centrello_0001.jpg")
+x = pffrocd.get_embedding("/home/chiem/pffrocd/lfw/German_Khan/German_Khan_0001.jpg")
+y = pffrocd.get_embedding("/home/chiem/pffrocd/lfw/Gina_Centrello/Gina_Centrello_0001.jpg")
 
 share0, share1 = pffrocd.create_shares(x, np.float32)
 share0prime, share1prime = pffrocd.create_shares(y, np.float32)
@@ -20,4 +24,7 @@ result = share0 - share1
 result_prime = share0prime - share1prime
 
 # cosine distance between the shares
-result_cosine = pffrocd.cosine_distance(result, result_prime)
+result_cosine = get_cos_dist_numpy(result, result_prime)
+print(result_cosine)
+
+print(get_cos_dist_numpy(x,y))
