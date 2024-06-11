f2607bdf887cdad544355170df86d5ae58386496
Chiem Stevens <c.p.stevens@student.tue.nl>
2024-06-11 11:33:11 +0200

    remove normalization and sent quantization to circuit

--- pyscripts/test-ar_circuit-calcs.py
+++ pyscripts/test-ar_circuit-calcs.py
@@ -18,16 +18,15 @@ NUMPY_DTYPE = np.float32
 # get two embeddings of different people
 x = pffrocd.get_embedding("/home/chiem/pffrocd/lfw/Adrian_McPherson/Adrian_McPherson_0001.jpg", dtype=NUMPY_DTYPE)
 y = pffrocd.get_embedding("/home/chiem/pffrocd/lfw/Adrian_McPherson/Adrian_McPherson_0002.jpg", dtype=NUMPY_DTYPE)
-print(x)
-quant_x = qt.scalar_quantisation_tensorrt(x)
-print(quant_x)
+x = qt.scalar_quantisation_tensorrt(x)
+y = qt.scalar_quantisation_tensorrt(y)
 
-x = x / np.linalg.norm(x)
+#x = x / np.linalg.norm(x)
 share0, share1 = pffrocd.create_shares(x, dtype=NUMPY_DTYPE)
 print(share0)
 print(share1)
 
-y = y / np.linalg.norm(y)
+#y = y / np.linalg.norm(y)
 share0prime, share1prime = pffrocd.create_shares(y, dtype=NUMPY_DTYPE)
 
 output = pffrocd.run_sfe_improved(x, y, y_0=share0, y_1=share1, x_0=share0prime, x_1=share1prime)
