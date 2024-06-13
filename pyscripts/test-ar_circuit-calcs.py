c436612f607a8f6f73a7617d1ba0d2952ed4d71b
Chiem <c.p.stevens@student.tue.nl>
2024-06-13 19:42:27 +0200

    test

--- pyscripts/test-ar_circuit-calcs.py
+++ pyscripts/test-ar_circuit-calcs.py
@@ -48,9 +48,11 @@ print("cosine distance: ", cosine_similarity(x, z))
 
 # multiply each item in x and y (which are np arrays) by 1000
 max_value = np.iinfo(np.uint32).max
+print("before max: ", x[0])
 x = x * 10000000000
 y = y * 10000000000
 z = z * 10000000000
+print("after max: ", x[0])
 
 # now convert x and y to int32
 x = np.array(x, dtype=np.uint32)
@@ -76,9 +78,11 @@ z = z / np.linalg.norm(z)
 
 # multiply each item in x and y (which are np arrays) by 65535
 max_value = np.iinfo(np.uint16).max
+print("before max: ", x[0])
 x = x * 100000
 y = y * 100000
 z = z * 100000
+print("after max: ", x[0])
 
 # now convert x and y to uint16
 x = np.array(x, dtype=np.uint16)
@@ -108,9 +112,11 @@ z = z / np.linalg.norm(z)
 
 # multiply each item in x and y (which are np arrays) by 65535
 max_value = np.iinfo(np.uint8).max
+print("before max: ", x[0])
 x = x * 1000
 y = y * 1000
 z = z * 1000
+print("after max: ", x[0])
 
 # now convert x and y to uint16
 x = np.array(x, dtype=np.uint8)
