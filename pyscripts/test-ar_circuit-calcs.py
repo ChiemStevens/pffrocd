a19975a3b0d39534010c1fa65380bff5f718e9fb
Chiem <c.p.stevens@student.tue.nl>
2024-06-13 16:03:47 +0200

    60k

--- pyscripts/test-ar_circuit-calcs.py
+++ pyscripts/test-ar_circuit-calcs.py
@@ -80,8 +80,8 @@ y = np.array(y, dtype=np.uint16)
 z = np.array(z, dtype=np.uint16)
 
 
-print("cosine distance: ", 1-(np.dot(x, y)/10000))
-print("cosine distance: ", 1-(np.dot(x, z)/10000))
+print("cosine distance: ", 1-(np.dot(x, y)/60000))
+print("cosine distance: ", 1-(np.dot(x, z)/60000))
 
 
 
