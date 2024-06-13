f5471ec574eb0ce65edb3f07e235dd96830c965d
Chiem <c.p.stevens@student.tue.nl>
2024-06-14 00:09:26 +0200

    verify

--- pyscripts/quant-test.py
+++ pyscripts/quant-test.py
@@ -8,6 +8,7 @@ import time
 import quantization as qt
 import sys
 import os
+import pandas as pd
 
 def get_two_random_images(same_person):
     """Get two random embeddings of either the same person or two different people out of all the images available"""
@@ -34,8 +35,46 @@ def get_two_random_images(same_person):
 
     return img1,img2
 
-img1, img2 = get_two_random_images(True)
-x = pffrocd.get_embedding(img1, dtype=np.float32)
-y = pffrocd.get_embedding(img2, dtype=np.float32)
-print(x)
-print(y)
+def cosine_similarity(v1, v2):
+    # Compute the cosine similarity
+    return 1- np.dot(v1, v2)
+
+def quant_uint32_cos_sim(x,y):
+    x = x * 1000
+    y = y * 1000
+    x = np.array(x, dtype=np.uint32)
+    y = np.array(y, dtype=np.uint32)
+    return 1 - (np.dot(x, y) / 1000000)
+
+def quant_uint16_cos_sim(x,y):
+    x = x * 100
+    y = y * 100
+    x = np.array(x, dtype=np.uint16)
+    y = np.array(y, dtype=np.uint16)
+    return 1 - (np.dot(x, y) / 10000)
+
+def quant_uint8_cos_sim(x,y):
+    x = x * 1000
+    y = y * 1000
+    x = np.array(x, dtype=np.uint8)
+    y = np.array(y, dtype=np.uint8)
+    return 1 - (np.dot(x, y) / 1000)
+
+# create a dataframe with pandas with columns for each cosine similarity
+df = pd.DataFrame(columns=['cosine_numpy', 'cosine_uint32', 'cosine_uint16', 'cosine_uint8'])
+for i in range(100):
+    img1, img2 = get_two_random_images(True)
+    x = pffrocd.get_embedding(img1, dtype=np.float32)
+    y = pffrocd.get_embedding(img2, dtype=np.float32)
+    x = x / np.linalg.norm(x)
+    y = y / np.linalg.norm(y)
+    print("cosine distance: ", cosine_similarity(x, y))
+    print("cosine distance uint32: ", quant_uint32_cos_sim(x, y))
+    print("cosine distance uint16: ", quant_uint16_cos_sim(x, y))
+    print("cosine distance uint8: ", quant_uint8_cos_sim(x, y))
+
+    # append values to the dataframe
+    df = df.append({'cosine_numpy': cosine_similarity(x, y), 'cosine_uint32': quant_uint32_cos_sim(x, y), 'cosine_uint16': quant_uint16_cos_sim(x, y), 'cosine_uint8': quant_uint8_cos_sim(x, y)}, ignore_index=True)
+
+# save the dataframe to a csv file
+df.to_csv('cosine_similarity.csv')
