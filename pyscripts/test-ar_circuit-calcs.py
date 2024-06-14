bdef1ed50f5646e58a9d26d6bda700385b7abb44
Chiem Stevens <c.p.stevens@student.tue.nl>
2024-06-14 12:39:18 +0200

    test

--- pyscripts/test-ar_circuit-calcs.py
+++ pyscripts/test-ar_circuit-calcs.py
@@ -9,26 +9,10 @@ import quantization as qt
 import sys
 import os
 
-# def evaluate_quantization(vector1, vector2, quantization_func):
-#     # Compute the cosine similarity before quantization
-#     #x = x / np.linalg.norm(x)
-#     before_quantization = cosine_similarity(vector1, vector2)
-
-#     # Quantize the vectors
-#     quantized_vector1 = quantization_func(vector1)
-#     quantized_vector2 = quantization_func(vector2)
-#     print(quantized_vector1)
-
-#     # Compute the cosine similarity after quantization
-#     after_quantization = cosine_similarity(quantized_vector1, quantized_vector2)
-
-#     # Return the cosine similarities before and after quantization
-#     return before_quantization, after_quantization
-
-# pffrocd.EXECUTABLE_PATH = "ABY/build/bin"
-# pffrocd.EXECUTABLE_NAME = 'cos_dist_int_scen_simd'
-# pffrocd.INPUT_FILE_NAME = f"input_{pffrocd.EXECUTABLE_NAME}.txt"
-# pffrocd.OUTPUT_FILE_NAME = f"/home/chiem/pffrocd"
+pffrocd.EXECUTABLE_PATH = "ABY/build/bin"
+pffrocd.EXECUTABLE_NAME = 'cos_dist_int_scen_simd'
+pffrocd.INPUT_FILE_NAME = f"input_{pffrocd.EXECUTABLE_NAME}.txt"
+pffrocd.OUTPUT_FILE_NAME = f"/home/chiem/pffrocd"
 NUMPY_DTYPE = np.float32
 
 def cosine_similarity(v1, v2):
@@ -66,48 +50,30 @@ y = pffrocd.get_embedding(img2, dtype=np.float32)
 x = x / np.linalg.norm(x)
 y = y / np.linalg.norm(y)
 
-# multiply each item in x and y (which are np arrays) by 65535
-print("cosine distance float32: ", 1- np.dot(x, y))
-print("before max: ", x[0])
-x = x * 1000
-y = y * 1000
-print("after max: ", x[0])
-print("test")
-# now convert x and y to uint16
-x = np.array(x, dtype=np.int8)
-y = np.array(y, dtype=np.int8)
-print("after max uint: ", x[0])
-# Compute the cosine similarity
-def cosine_similarity(v1, v2):
-    print(np.dot(v1, v2))
-    return 1 - (np.dot(v1, v2) / 200)
-
-print("cosine distance uint8: ", cosine_similarity(x, y))
-
 # before, after = evaluate_quantization(x,y,qt.scalar_quantisation_percentile)
 # print("BEFORE QUANTIZATION: ", before)
 # print("AFTER QUANTIZATION: ", after)
 
-# x = qt.scalar_quantisation_percentile(x)
-# y = qt.scalar_quantisation_percentile(y)
+x = qt.scalar_quantisation_percentile(x)
+y = qt.scalar_quantisation_percentile(y)
 
-# share0, share1 = pffrocd.create_shares(np.array(x, dtype=NUMPY_DTYPE), NUMPY_DTYPE, True)
-# share0prime, share1prime = pffrocd.create_shares(np.array(y, dtype=NUMPY_DTYPE), NUMPY_DTYPE, True)
+share0, share1 = pffrocd.create_shares(np.array(x, dtype=NUMPY_DTYPE), NUMPY_DTYPE, True)
+share0prime, share1prime = pffrocd.create_shares(np.array(y, dtype=NUMPY_DTYPE), NUMPY_DTYPE, True)
 
-# print("og share1 prime", share0prime)
-# share0 = np.array(share0, dtype=np.uint32)
-# share1 = np.array(share1, dtype=np.uint32)
-# share0prime = np.array(share0prime, dtype=np.uint32)
-# print(share0prime)
-# share1prime = np.array(share1prime, dtype=np.uint32)
-# x = np.array(x, dtype=np.uint32)
-# y = np.array(y, dtype=np.uint32)
-# output = pffrocd.run_sfe_improved(x, y, y_0=share0, y_1=share1, x_0=share0prime, x_1=share1prime)
-# print(output.stdout)
+print("og share1 prime", share0prime)
+share0 = np.array(share0, dtype=np.uint32)
+share1 = np.array(share1, dtype=np.uint32)
+share0prime = np.array(share0prime, dtype=np.uint32)
+print(share0prime)
+share1prime = np.array(share1prime, dtype=np.uint32)
+x = np.array(x, dtype=np.uint32)
+y = np.array(y, dtype=np.uint32)
+output = pffrocd.run_sfe_improved(x, y, y_0=share0, y_1=share1, x_0=share0prime, x_1=share1prime)
+print(output.stdout)
 
-# print("NUMPY COS_DIST:")
-# print(pffrocd.get_cos_dist_numpy(x,y))
-# print(np.dot(x,y))
+print("NUMPY COS_DIST:")
+print(pffrocd.get_cos_dist_numpy(x,y))
+print(np.dot(x,y))
 # # the dot product written out
 # sum = 0
 # for i in range(0, len(x)):
