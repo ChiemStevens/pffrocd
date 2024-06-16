33017bf8c3ebff12adfbf75055fc2d2c2affae6c
Chiem <c.p.stevens@student.tue.nl>
2024-06-16 19:17:16 +0200

    scalar

--- pyscripts/master-quant.py
+++ pyscripts/master-quant.py
@@ -143,6 +143,7 @@ def run_test():
             # split the string in two, parts before | and after
             s = stdout.split('|')
             scalar = s[0].strip("[]")
+            scalar = s[0][1:]
             logger.info(f"scalar: {scalar}")
             # Remove the brackets and split the string into a list of strings
             # Convert the list of strings into a list of floats
@@ -156,7 +157,7 @@ def run_test():
             if stderr != '':
                 logger.error("REMOTE EXECUTION OF COMMAND FAILED")
                 logger.error(stderr)
-            pffrocd.write_share_to_remote_file(client_ip, client_username, master_key_path, f"{client_exec_path}/share1scalar_y.txt", scalar[0])
+            pffrocd.write_share_to_remote_file(client_ip, client_username, master_key_path, f"{client_exec_path}/share1scalar_y.txt", np.float32(scalar))
             pffrocd.write_share_to_remote_file(client_ip, client_username, master_key_path, f"{client_exec_path}/share1prime.txt", shareprime)
             
             # send the files with embeddings to the client and server
