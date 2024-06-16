71f16752fb21bb8c69a506608a18f1663c682470
Chiem <c.p.stevens@student.tue.nl>
2024-06-16 19:03:20 +0200

    quant

--- pyscripts/master-quant.py
+++ pyscripts/master-quant.py
@@ -147,6 +147,8 @@ def run_test():
             # Convert the list of strings into a list of floats
             logger.info(f"s1 looks like: {s[1]}")
             s[1] = s[1].strip("[]").split()
+            # remove first item from s[1]
+            s[1] = s[1][1:]
             logger.info(f"s1 looks like: {s[1]}")
             s = [np.int32(i) for i in s[1]]
             # Convert the list of floats into a numpy array
