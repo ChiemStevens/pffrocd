020dfc11447b8e26279e4f598feab62ff4a0ddc5
Chiem <c.p.stevens@student.tue.nl>
2024-06-16 19:00:56 +0200

    test

--- pyscripts/master-quant.py
+++ pyscripts/master-quant.py
@@ -146,9 +146,7 @@ def run_test():
             # Remove the brackets and split the string into a list of strings
             # Convert the list of strings into a list of floats
             logger.info(f"s1 looks like: {s[1]}")
-            s[1] = s[1].strip("[]").strip()
-            logger.info(f"s1 looks like: {s[1]}")
-            s[1] = s[1].strip("[]")
+            s[1] = s[1].strip("[]").split()
             logger.info(f"s1 looks like: {s[1]}")
             s = [np.int32(i) for i in s[1]]
             # Convert the list of floats into a numpy array
