6bb4d71b1bb74a165a78b4be6c4ab3f8f95c1c6f
Chiem <c.p.stevens@student.tue.nl>
2024-06-16 18:58:24 +0200

    strip split print

--- pyscripts/master-quant.py
+++ pyscripts/master-quant.py
@@ -145,7 +145,10 @@ def run_test():
             scalar = s[0].strip()
             # Remove the brackets and split the string into a list of strings
             # Convert the list of strings into a list of floats
+            logger.info(f"s1 looks like: {s[1]}")
             s[1] = s[1].strip("[]").strip()
+            logger.info(f"s1 looks like: {s[1]}")
+            s[1] = s[1].strip("[]")
             logger.info(f"s1 looks like: {s[1]}")
             s = [np.int32(i) for i in s[1]]
             # Convert the list of floats into a numpy array
