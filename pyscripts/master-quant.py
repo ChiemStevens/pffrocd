2236f9dd303301e9e76838c1ff9863a5ec0fbd6f
Chiem <c.p.stevens@student.tue.nl>
2024-06-16 19:20:32 +0200

    scalar

--- pyscripts/master-quant.py
+++ pyscripts/master-quant.py
@@ -143,7 +143,8 @@ def run_test():
             # split the string in two, parts before | and after
             s = stdout.split('|')
             scalar = s[0].strip("[]").split()
-            scalar = s[0][1:]
+            # remove the last item from scalar
+            scalar = scalar[:-1]
             logger.info(f"scalar: {scalar}")
             # Remove the brackets and split the string into a list of strings
             # Convert the list of strings into a list of floats
